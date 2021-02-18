1b7b9e7bf9f4401fc6c2afe010838cf262abccd6
Vítor Marques de Faria <vtrmrx@gmail.com>
2021-02-18 15:41:12 -0300

    adiciona link a popover de info

--- assets/js/infoPopovers.js
+++ assets/js/infoPopovers.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
   let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
+    return new bootstrap.Popover(popoverTriggerEl, {
+      trigger: "focus",
+      container: "body",
+      boundary: "viewport",
+      html: true,
+      sanitize: false
+    })
+  });
+  document.addEventListener('mousedown', event => {
+    event.path.forEach(function(element, index) {
+      if(element.classList && element.classList.contains('popover-body')) {
+        event.preventDefault();
+        return;
+      }
+    });
+  });
 });
